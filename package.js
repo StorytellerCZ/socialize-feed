5d6b427c6bcc1726e0f6eb8e38fd6c103a839ab9
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-05 00:09:12 +0200

    Initial Meteor 3 upgrade work

--- package.js
+++ package.js
@@ -2,21 +2,21 @@
 Package.describe({
     name: 'socialize:feed',
     summary: 'A package for impmementing a social network style news feed',
-    version: '1.0.5',
+    version: '2.0.0',
     git: 'https://github.com/copleykj/socialize-feed.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['1.10.2', '2.3']);
+    api.versionsFrom(['2.8.1', '3.0-rc.0']);
 
     api.use([
         'check',
-        'reywood:publish-composite@1.7.3',
-        'socialize:user-blocking@1.0.6',
-        'socialize:postable@1.0.4',
+        'reywood:publish-composite@1.8.9',
+        'socialize:user-blocking@2.0.0',
+        'socialize:postable@2.0.0',
     ]);
 
-    api.use('socialize:friendships@1.1.2', { weak: true });
+    api.use('socialize:friendships@2.0.0', { weak: true });
 
     api.mainModule('server/server.js', 'server');
     api.mainModule('common/common.js', 'client');
